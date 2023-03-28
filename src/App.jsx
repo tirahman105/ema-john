f0729a0e3a9fe8982f6034aa8764e3d3545f5e0a
tirahman105 <tirahman105@gmail.com>
2023-03-28 17:33:52 +0600

    product load from public folder

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header/Header'
+import Shop from './components/Shop/Shop'
+
 
 
 function App() {
@@ -12,6 +14,7 @@ function App() {
     <div className="App">
      
      <Header></Header>
+     <Shop></Shop>
     </div>
   )
 }
