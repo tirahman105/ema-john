28228348b8b98c78624b6d828d4c3e30ad2caea7
tirahman105 <tirahman105@gmail.com>
2023-03-28 19:36:11 +0600

    cart state setup, react font awsome, display icon

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -4,11 +4,20 @@ import './Shop.css';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]) 
+
+
     useEffect(() => {
         fetch('products.json')
         .then(res => res.json())
         .then(data => setProducts(data))
     }, [])
+
+    const handleAddToCart = (product) =>{
+        // console.log('Added', product)
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
     return (
         <div className='shop-container'>
             <div className="products-container">
@@ -16,11 +25,13 @@ const Shop = () => {
                     products.map(product => <Product
                     key={product.id}
                     product= {product}
+                    handleAddToCart= {handleAddToCart}
                     ></Product>)
                 }
             </div>
             <div className="cart-container">
                 <h2>Order Summary </h2>
+                <p>Selected Items: {cart.length}</p>
             </div>
             
         </div>
