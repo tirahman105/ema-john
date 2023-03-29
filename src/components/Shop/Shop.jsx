f6c5cb437e1b9484126fc67a9efbec7e614db48e
tirahman105 <tirahman105@gmail.com>
2023-03-29 22:13:49 +0600

    how data from cart

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -42,7 +42,24 @@ const Shop = () => {
 
     const handleAddToCart = (product) =>{
         // console.log('Added', product)
-        const newCart = [...cart, product];
+        // const newCart = [...cart, product];
+
+        let newCart = [];
+
+        // if product does not exist in the cart , then set the quantity = 1
+        // if exist update quantity by 1
+
+        const exists = cart.find(pd => pd.id === product.id);
+
+        if(!exists){
+            product.quantity = 1;
+            newCart=[...cart, product]
+        }else{
+            exists.quantity = exists.quantity + 1;
+            const remaining = cart.filter(pd => pd.id !== product.id);
+            newCart = [...remaining, exists];
+        }
+
         setCart(newCart);
         addToDb(product.id);
     }
